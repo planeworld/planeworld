778ef7ded3041e7be5a6fa9b6a8d1edf463c9501
Sven Klomp <mail@klomp.eu>
2014-05-13 22:23:28 +0200

    Change conversion to SI

--- tools/convert.js
+++ tools/convert.js
@@ -1,23 +1,23 @@
 
 
 module.exports.ephemeris2vector = function(eph) {
-    // True anomaly, nu (degrees)
-    var TA = eph.TA;
+    // True anomaly, nu (rad)
+    var TA = deg2rad(eph.TA);
 
-    // Argument of Perifocus, w (degrees)                                
-    var W = eph.W;
+    // Argument of Perifocus, w (rad)                                
+    var W = deg2rad(eph.W);
 
     // Eccentricity, e                                                   
     var EC = eph.EC;
 
-    // Mean motion, n (degrees/sec)
-    var N = eph.N;
+    // Mean motion, n (rad/sec)
+    var N = deg2rad(eph.N);
 
-    // Semi-major axis, a (km)                                           
-    var A = eph.A;
+    // Semi-major axis, a (m)                                           
+    var A = eph.A*1000;
 
-    // Longitude of Ascending Node, OMEGA, (degrees) 
-    var OM = eph.OM;
+    // Longitude of Ascending Node, OMEGA, (rad) 
+    var OM = deg2rad(eph.OM);
 
 
     // -------------------------------
@@ -28,18 +28,18 @@ module.exports.ephemeris2vector = function(eph) {
     
     
     // Radius
-    var r = A * (1-EC*EC) / (1 + EC * Math.cos(deg2rad(TA)));
+    var r = A * (1-EC*EC) / (1 + EC * Math.cos(TA));
 
     // X and Y coordinates of object
-    data.position[0] = r * Math.cos(deg2rad(TA+W+OM));
-    data.position[1] = r * Math.sin(deg2rad(TA+W+OM));
+    data.position[0] = r * Math.cos(TA+W+OM);
+    data.position[1] = r * Math.sin(TA+W+OM);
 
     // Velocities
-    var Vr = deg2rad(N) * EC * Math.sin(deg2rad(TA)) * r / Math.sqrt(1-EC*EC);
-    var Vnu = deg2rad(N) * A;
+    var Vr = N * EC * Math.sin(TA) * r / Math.sqrt(1-EC*EC);
+    var Vnu = N * A;
 
-    data.velocity[0] = Vr * Math.cos(deg2rad(TA+W+OM)) - Vnu * Math.sin(deg2rad(TA+W+OM));
-    data.velocity[1] = Vr * Math.sin(deg2rad(TA+W+OM)) + Vnu * Math.cos(deg2rad(TA+W+OM));
+    data.velocity[0] = Vr * Math.cos(TA+W+OM) - Vnu * Math.sin(TA+W+OM);
+    data.velocity[1] = Vr * Math.sin(TA+W+OM) + Vnu * Math.cos(TA+W+OM);
 
     function deg2rad(degree) {
         return 2*Math.PI/360 * degree;
