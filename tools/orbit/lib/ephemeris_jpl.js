6c5e4922aa63eb891401656c007bbc2c00cbc59e
Sven Klomp <mail@klomp.eu>
2014-05-16 22:51:11 +0200

    Create xml from jpl data

--- tools/orbit/lib/ephemeris_jpl.js
+++ tools/orbit/lib/ephemeris_jpl.js
@@ -4,20 +4,30 @@ var HORIZONS_URI = "horizons.jpl.nasa.gov";
 var HORIZONS_PORT = 6775;
 
 var objects = {}
+objects["sun"]=10;
+objects["mercury"]=199;
+objects["venus"]=299;
 objects["earth"]=399;
+objects["mars"]=499;
+objects["jupiter"]=599;
+objects["saturn"]=699;
+objects["uranus"]=799;
+objects["neptune"]=899;
+
 objects["moon"]=301;
 
-var object = {}
-object.ephemeris = {};
 
 
 module.exports.get = function(object_name, date, callback){
+    var object;
+    
     var client = new Telnet(HORIZONS_URI, HORIZONS_PORT);
     client.connect();
     client.readUntil(new RegExp("Horizons> "), function(err, res) {
         client.write(objects[object_name] + "\n");
         client.readUntil(new RegExp("<cr>: "), function(err, res) {
-            parseObjectData(res);
+            object = parseObjectData(res);
+            object.name = object_name;
             client.write("E\n");
             client.readUntil(new RegExp("] : "), function(err, res) {
                 client.write("e\n", "ASCII");
@@ -44,7 +54,7 @@ module.exports.get = function(object_name, date, callback){
                                                         client.readUntil(new RegExp("] : "), function(err, res) {
                                                             client.write("ABS\n", "ASCII");
                                                             client.readUntil(new RegExp("\\? : "), function(err, res) {
-                                                                parseEphemeris(res);
+                                                                object.ephemeris = parseEphemeris(res);
                                                                 client.close();
                                                                 callback(null, object);
                                                             });
@@ -67,21 +77,72 @@ module.exports.get = function(object_name, date, callback){
 
         
 function parseObjectData(data) {
-    var pattern_radius = new RegExp(' Mean radius, km += ([0-9]*.[0-9]*)');
-    var pattern_mass = new RegExp(' Mass, 10\\^([0-9]+) kg = ([0-9]*.[0-9]*)');
+    var parser_radius = new Parser();
+
+    parser_radius.add(' Mean radius, km += +([0-9]*.[0-9]*)', function(match) {
+       return parseFloat(match[1])*1000; 
+    });
+    parser_radius.add(' Radius, km += +([0-9]*.[0-9]*)', function(match) {
+       return parseFloat(match[1])*1000; 
+    });
+    parser_radius.add(' Mean radius \\(km\\) += +([0-9]*.[0-9]*)', function(match) {
+       return parseFloat(match[1])*1000; 
+    });
+    parser_radius.add(' Equat. radius \\(1 bar\\) += +([0-9]*.[0-9]*).* km', function(match) {
+       return parseFloat(match[1])*1000; 
+    });
     
-    var match;
-    match = pattern_radius.exec(data);
-    if (match) {
-        object.radius=parseFloat(match[1])*1000;
-        console.log("Radius = "+ object.radius);
-    }
-    match = pattern_mass.exec(data);
-    if (match) {
-        object.mass=parseFloat(match[2]+"E"+match[1]);
-        console.log("Mass = "+ object.mass);
-    }
+
+    var parser_mass = new Parser();
+
+    parser_mass.add(' Mass, 10\\^([0-9]+) kg = +([0-9]*.[0-9]*)', function(match) {
+       return parseFloat(match[2]+"E"+match[1]);
+    });
+    parser_mass.add(' Mass \\(10\\^([0-9]+) kg *\\) += +([0-9]*.[0-9]*)', function(match) {
+       return parseFloat(match[2]+"E"+match[1]);
+    });
+
+    
+    var object = {};
+    
+    var radius = parser_radius.parse(data);
+    if (radius)
+        object.radius = radius;
+        
+    var mass = parser_mass.parse(data);
+    if (mass)
+        object.mass = mass;
+    
+    return object;
 };
+
+var Parser = function() {
+    this.parsers = [];
+}
+
+Parser.prototype.add = function(regexp, calc) {
+    this.parsers.push({
+        pattern: new RegExp(regexp),
+        calc: calc
+    });
+};
+
+Parser.prototype.parse = function(data) {
+    var result = null;
+    var match;
+    
+    for (var i=0; i<this.parsers.length; i++) {
+        match = this.parsers[i].pattern.exec(data);
+        if (match) {
+            result = this.parsers[i].calc(match);
+            break;
+        }
+    }
+    return result;
+}
+
+
+
 
 function parseEphemeris(data) {
     var pattern_EC = new RegExp('EC= ([0-9]+.[0-9]+(E.[0-9]+)?) ');
@@ -91,42 +152,40 @@ function parseEphemeris(data) {
     var pattern_A = new RegExp('A = ([0-9]+.[0-9]+(E.[0-9]+)?) ');
     var pattern_OM = new RegExp('OM= ([0-9]+.[0-9]+(E.[0-9]+)?) ');
 
-
+    var ephemeris = {}
     match = pattern_EC.exec(data);
     if (match) {
-        object.ephemeris.EC=parseFloat(match[1]);
-        console.log("EC = "+ object.ephemeris.EC);
+        ephemeris.EC=parseFloat(match[1]);
     }
 
     match = pattern_W.exec(data);
     if (match) {
-        object.ephemeris.W=parseFloat(match[1]);
-        console.log("W = "+ object.ephemeris.W);
+        ephemeris.W=parseFloat(match[1]);
     }
 
     match = pattern_N.exec(data);
     if (match) {
-        object.ephemeris.N=parseFloat(match[1]);
-        console.log("N = "+ object.ephemeris.N);
+        ephemeris.N=parseFloat(match[1]);
     }
 
     match = pattern_TA.exec(data);
     if (match) {
-        object.ephemeris.TA=parseFloat(match[1]);
-        console.log("TA= "+ object.ephemeris.TA);
+        ephemeris.TA=parseFloat(match[1]);
     }
 
     match = pattern_A.exec(data);
     if (match) {
-        object.ephemeris.A=parseFloat(match[1]);
-        console.log("A= "+ object.ephemeris.A);
+        ephemeris.A=parseFloat(match[1]);
     }
     
     match = pattern_OM.exec(data);
     if (match) {
-        object.ephemeris.OM=parseFloat(match[1]);
-        console.log("OM= "+ object.ephemeris.OM);
+        ephemeris.OM=parseFloat(match[1]);
     }
-        
+    
+    return ephemeris;
 };
 
+module.exports.objects = objects;
+module.exports._parseObjectData = parseObjectData;
+module.exports._parseEphemeris = parseEphemeris;
---
+++ tools/orbit/lib/xml.js
@@ -0,0 +1,28 @@
+
+
+module.exports.object2xml = function(object) {
+    var xml = '<!DOCTYPE PlaneworldML>\n';
+    xml += '<object type="RigidBody">\n';
+    xml += '    <core\n';
+    xml += '        name="'+ object.name +'"\n';
+    xml += '        mass="'+ object.mass +'"\n';
+    xml += '        origin_x="'+ object.position[0] +'"\n';
+    xml += '        origin_y="'+ object.position[1] +'"\n';
+    xml += '        velocity_x="'+ object.velocity[0] +'"\n';
+    xml += '        velocity_y="'+ object.velocity[1] +'"\n';
+    xml += '        dynamics="true"\n';
+    xml += '        gravity="true"\n';
+    xml += '    />\n';
+    xml += '    <shape\n';
+    xml += '        type="Cirlce"\n';
+    xml += '        radius="'+ object.radius +'"\n';
+    xml += '        center_x="0.0"\n';
+    xml += '        center_y="0.0">\n';
+    xml += '        <visuals type="Circle"/>\n';
+    xml += '    </shape>\n';
+    xml += '</object>\n';
+
+    return xml;
+};
+
+
