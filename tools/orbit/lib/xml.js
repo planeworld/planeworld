2c3287e86a5de551b32094cc1ad1a22d3583abbb
Sven Klomp <mail@klomp.eu>
2014-05-18 18:31:44 +0200

    Adapts xml export to new structure

--- tools/orbit/lib/xml.js
+++ tools/orbit/lib/xml.js
@@ -3,6 +3,7 @@
 module.exports.object2xml = function(object) {
     var xml = '<!DOCTYPE PlaneworldML>\n';
     xml += '<object type="RigidBody">\n';
+
     xml += '    <core\n';
     xml += '        name="'+ object.name +'"\n';
     xml += '        mass="'+ object.mass +'"\n';
@@ -14,10 +15,14 @@ module.exports.object2xml = function(object) {
         xml += '        velocity_x="'+ object.velocity[0] +'"\n';
         xml += '        velocity_y="'+ object.velocity[1] +'"\n';
     }
-    xml += '        angle_velocity="'+ object.rotation * 2*Math.PI/360 +'"\n';
     xml += '        dynamics="true"\n';
     xml += '        gravity="true"\n';
     xml += '    />\n';
+
+    xml += '    <body_core\n';
+    xml += '        angle_velocity="'+ object.rotation * 2*Math.PI/360 +'"\n';
+    xml += '    />\n';
+
     xml += '    <shape\n';
     xml += '        type="Circle"\n';
     xml += '        radius="'+ object.radius +'"\n';
