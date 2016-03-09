f856acf13706fec9dca57cdb3965786ae6bb5166
bfeld <planeworld@bfeld.eu>
2016-03-09 14:55:36 +0100

    Close #7. Add parameter to adjust camera resolution in m/px

--- pw_io/import/xml_importer.cpp
+++ pw_io/import/xml_importer.cpp
@@ -551,6 +551,7 @@ void CXMLImporter::createCamera(const pugi::xml_node& _Node)
                               _Node.attribute("position_y").as_double());
         m_pCamera->setViewport(_Node.attribute("viewport_width").as_int(),
                               _Node.attribute("viewport_height").as_int());
+        m_pCamera->zoomTo(GRAPHICS_PX_PER_METER/_Node.attribute("m_per_px").as_double());
 //         if (checkAttributeBool(_Node, "enable_angle_hook", true)) m_pCamera->enableAngleHook();
 //         else m_pCamera->disableAngleHook();
         
